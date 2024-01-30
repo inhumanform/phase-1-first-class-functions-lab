5b1f6b4d4937e11e29932989615938e21faac60a
Drew <dch528@gmail.com>
2024-01-30 14:35:01 -0500

    Drew Hairston First Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+    console.log(firstTwoDrivers);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+    console.log(lastTwoDrivers);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier; 
+};
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, whichDrivers) {
+    return whichDrivers(drivers);
+
+}
+
+const firstTwoDrivers = returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+console.log(firstTwoDrivers);
